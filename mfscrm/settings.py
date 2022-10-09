4a18222739133a1969b7139b209713f6c5305d7d
ahayworth1 <93269307+ahayworth1@users.noreply.github.com>
2022-10-09 08:33:10 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-vherc04dh&c21dsz^4q-g^ha3z-vw+tqo$^rgfh4bcj&&o8)$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
